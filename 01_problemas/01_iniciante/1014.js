93f4e353dd0cd7b1b1b4d7c8194b39a1b57d7e4f
Leonardo Santos <leonardojosantos@gmail.com>
2024-09-01 22:25:22 -0300

    problema 1015

---
+++ 01_problemas/01_iniciante/1014.js
@@ -0,0 +1,14 @@
+const input = `500
+35.0`;
+
+const lines = input.split('\n');
+
+const [
+    distanciaTotalPercorrida, totalCombustivelGasto
+] = lines;
+
+const consumoMediaAutomovel = (Number(distanciaTotalPercorrida) / Number(totalCombustivelGasto));
+
+console.log(
+    `${consumoMediaAutomovel.toFixed(3)} km/l`
+);
